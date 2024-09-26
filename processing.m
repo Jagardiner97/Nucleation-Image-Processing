87208a44a204104ef4425cffb74489e2fbec1b5f
= <austin.gardiner97@gmail.com>
2024-09-26 14:42:54 -0600

    updated code comments and added automatic image counting

--- processing.m
+++ processing.m
@@ -1,25 +1,37 @@
+% clear workspace and start calculating runtime
 clear;
 tic;
 
-%% Define Sensitivity
-sensitivity = 12;
+%% Change the Image Directory
+directory = "5/"; %path to folder where the images are stored
 
-%% Get the Number of Pictures
-num_pictures = 233;
-np_str = string(num_pictures);
-file_type = '.jpg';
-directory = "5/";
-
-%% Read crop information
+%% Read Crop Information
+% change xmin, ymin, width, and height
+% can use calibration.m to find these numbers
 xmin = 0;
 ymin = 500;
 width = 4000;
 height = 2500;
-circle_radii_range = [35, 100];
+circle_radii_range = [35, 100]; %adjust if large/small droplets are excluded
 circle_sensitivity = 0.87;
 circle_edge_threshold = 0.037;
 
+%% Get the Number of Pictures
+file_type = '.jpg'; %if filetype changes adjust that here
+file_search = strcat("*", file_type);
+
+% search the target directory for images of the specified filetype
+a = dir(fullfile(directory, file_search));
+a = a(~startsWith({a.name}, '._')); %remove cache files
+num_pictures = numel(a);
+np_str = string(num_pictures);
+
+%% Define Sensitivity
+% controls the amount of change in mean pixel value to indicate freezing
+sensitivity = 12;
+
 %% Select the Calibration Image
+% selects the last image in the specified directory based on num_pictures
 cal_pic_name = strcat(directory, np_str, file_type);
 cal_pic = imread(cal_pic_name);
 cal_pic = imcrop(cal_pic, [xmin ymin width height]);
@@ -29,13 +41,19 @@ num_drops = length(radii);
 
 %% Remove Overlapping Droplets
 ave_rad = mean(radii);
+
+% finds the index of any overlapping droplets
 overlapping = [];
 for i = 1:num_drops
     if not(ismember(i, overlapping))
         for j = 1:num_drops
             if (j ~= i) && (not(ismember(j, overlapping)))
+                % calculate x and y center to center distances
                 dx = centers(i,1) - centers(j,1);
                 dy = centers(i,2) - centers(j,2);
+                
+                % calculate total center to center distance and compare to
+                % average radius to determine overlap
                 dist = sqrt(dx^2 + dy^2);
                 if dist < ave_rad * 1.2
                     overlapping = [overlapping; j];
@@ -45,11 +63,15 @@ for i = 1:num_drops
     end    
 end
 overlap_descending = sort(overlapping, 'descend');
+
+% removes overlapping droplets
 for i = 1:length(overlap_descending)
     remove = overlap_descending(i);
     centers(remove,:) = [];
     radii(remove,:) = [];
 end
+
+% recalculate the number of droplets and combine centers/radii
 num_drops = length(radii);
 centers_and_radii = [centers, radii];
 
@@ -90,6 +112,7 @@ for i = 1:number
     last_row = end_index;
 end
 
+% update reorganized centers and radii
 centers = vsorted(:, 1:2);
 radii = vsorted(:, 3);
 
@@ -156,13 +179,21 @@ for i = 1:num_drops
     report(i,2) = droplet(i).status;
 end
 
+%% Show the Calibration Image with Droplet Borders
 imshow(cal_gray);
 viscircles(centers, radii);
 
 %% Create the Report
+% check if there is a results directory inside the image directory
 results_dir_name = strcat(directory, "results");
-mkdir (results_dir_name)
+if ~exist(result_dir_name, 'dir')
+    % make a results folder if none found
+    mkdir (results_dir_name)
+end
+
+% write the report and save the droplets image
 writematrix(report, strcat(results_dir_name, '/report.csv'));
 saveas(gcf, strcat(results_dir_name, '/droplets.png'));
 
+% display runtime
 toc;
